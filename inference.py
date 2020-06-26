15b757815485852a9c0499bf6bf0f35d53b55dfc
Oricha Abdulrasheed Muhammed <am4rash@gmail.com>
2020-06-26 13:40:18 +0100

    Updated TODO functions

--- inference.py
+++ inference.py
@@ -36,32 +36,71 @@ class Network:
 
     def __init__(self):
         ### TODO: Initialize any class variables desired ###
+        self.plugin = None
+        self.network = None
+        self.input_blob = None
+        self.output_blob = None
+        self.exec_network = None
+        self.infer_request = None
 
-    def load_model(self):
-        ### TODO: Load the model ###
-        ### TODO: Check for supported layers ###
+    def load_model(self, model_path, device="CPU", cpu_extension = None):
+        model_weights = model_path[:-3]+"bin"
+        self.plugin = IECore()
+        self.network = IENetwork(model = model_path, weights = model_weights)
+        
         ### TODO: Add any necessary extensions ###
+        if cpu_extension and "CPU" in device:
+            self.plugin.add_extension(cpu_extension, device)
+        
+          ### TODO: Load the model ###
+        self.exec_network = self.plugin.load_network(self.network, device)
+            
+        # Check Network layer support
+        if "CPU" in device:
+            supported_layers = self.plugin.query_network(self.network, "CPU")
+            not_supported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+        if len(not_supported_layers) != 0:
+            log.error("Following layers are not supported by the plugin for the specified device {}:\n {}".
+                          format(device, ', '.join(not_supported_layers)))
+            log.error(
+                    "Please try to specify cpu extensions library path in sample's command line parameters using -l "
+                    "or --cpu_extension command line argument")
+            sys.exit(1)
+
+       
+        self.input_blob = next(iter(self.network.inputs))
+        self.output_blob = next(iter(self.network.outputs))
+        
         ### TODO: Return the loaded inference plugin ###
+        return self.exec_network
+       
+        ### TODO: Check for supported layers ###
+       
+        
         ### Note: You may need to update the function parameters. ###
-        return
+#         return
 
     def get_input_shape(self):
         ### TODO: Return the shape of the input layer ###
-        return
+        input_shape = self.network.inputs[self.input_blob].shape
+#         print("LENGTH {}\n".format(input_shape))
+        return input_shape
 
-    def exec_net(self):
+    def exec_net(self, inf_input):
         ### TODO: Start an asynchronous request ###
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
+        result = self.exec_network.start_async(request_id=0, inputs={self.input_blob:inf_input})
+        return result
 
     def wait(self):
         ### TODO: Wait for the request to be complete. ###
         ### TODO: Return any necessary information ###
         ### Note: You may need to update the function parameters. ###
-        return
+        inf_status = self.exec_network.requests[0].wait(-1)
+        return inf_status
 
     def get_output(self):
         ### TODO: Extract and return the output results
         ### Note: You may need to update the function parameters. ###
-        return
+        return self.exec_network.requests[0].outputs[self.output_blob]
--- main.py
+++ main.py
@@ -70,8 +70,8 @@ def build_argparser():
 
 def connect_mqtt():
     ### TODO: Connect to the MQTT client ###
-    client = None
-
+    client = mqtt.Client()
+    client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
     return client
 
 
@@ -88,6 +88,75 @@ def infer_on_stream(args, client):
     infer_network = Network()
     # Set Probability threshold for detections
     prob_threshold = args.prob_threshold
+    infer_network.load_model(args.model, args.device,  args.cpu_extension)
+    n,c,h,w = infer_network.get_input_shape() #n, c,
+    total_people = 0
+    current_count = 0
+    prev_count = 0
+    duration = 0
+    new_count = False
+    if args.input == "CAM":
+        args.input=0
+    cap = cv2.VideoCapture(args.input)
+    cap.open(args.input)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    frame_wait_duration = 0
+    start_time = 0
+    
+    while cap.isOpened():
+        flag, frame = cap.read()
+        
+        if flag== False:
+            break
+        key_pressed = cv2.waitKey(60)
+        
+        pre_pro_frame = cv2.resize(frame, (w, h))
+        pre_pro_frame = pre_pro_frame.transpose((2,0,1))
+        pre_pro_frame = pre_pro_frame.reshape((n,c,h, w))#n, c,
+        inf_start = time.time()
+        
+        infer_network.exec_net(pre_pro_frame)
+        if infer_network.wait() == 0:
+            det_time = time.time() - inf_start
+            result = infer_network.get_output()
+            current_count = 0
+            for boxes in result[0][0]:
+                if boxes[2] >= prob_threshold :
+                    xmin = int(boxes[3]*width)
+                    ymin = int(boxes[4]*height)
+                    xmax = int(boxes[5]*width)
+                    ymax = int(boxes[6]*height)
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax),(256, 256, 256), 1)
+                    frame_wait_duration +=1   
+                    current_count += 1
+            inf_time_message = "Inference time: {:.3f}ms" \
+                .format(det_time * 1000)
+            cv2.putText(frame, inf_time_message, (15, 15),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
+            
+            if current_count > prev_count and frame_wait_duration%5==0:#and frame_wait_duration == 5
+                start_time = time.time()
+                total_people += current_count - prev_count
+                client.publish("person", json.dumps({"total":total_people}))
+    
+            if current_count < prev_count and frame_wait_duration%5==0:
+                duration = int(time.time()-start_time)
+                client.publish("person/duration",json.dumps({"duration":duration}))
+                
+            
+                
+            client.publish("person",json.dumps({"count":current_count}))
+            prev_count = current_count
+            if key_pressed == 27:
+                break
+        
+        sys.stdout.buffer.write(frame)
+        sys.stdout.flush()
+    cap.release()
+    cv2.destroyAllWindows()
+    client.disconnect()
+                                                    
 
     ### TODO: Load the model through `infer_network` ###
 
